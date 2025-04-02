83027ee14ede1123a29476bfa08a83bc5657af88
lim ho <dlagh123@gmail.com>
2025-04-02 21:42:57 +0900

    cors

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const todoRouter = require("./routes/todo");
 const app = express();
 
 app.use(morgan("combined", { stream: accessLogStream }));
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(cors()); // 모든 도메인에서 요청 허용
 
 app.use(express.json());
 app.use("/api/todo", todoRouter);
