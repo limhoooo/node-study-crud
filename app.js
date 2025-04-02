f96262d7771cd5f24941ba0b4eaf34522981b1f1
lim ho <dlagh123@gmail.com>
2025-04-02 21:43:24 +0900

    cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const morgan = require("morgan");
 const accessLogStream = require("./utils/log");
 
